ab358c12fb171b4eb8a544e24ffd6f70dbc03f7c
jonathanriese <jonathanriese@gmail.com>
2021-04-15 22:45:33 +0200

    responsive nav, smaller CSS changes

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-console.log('hello');
+
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > window.innerHeight);
+    const height = window.innerHeight - 70;
+    header.classList.toggle('scrolled', window.scrollY > height);
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
